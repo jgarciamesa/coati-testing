09dc11f26e004f6cf0c9d0f9bc4a76bbfe66e987
Reed A. Cartwright <cartwright@asu.edu>
2024-01-21 19:04:32 -0700

    Add missing variables to align.mk

--- align.mk
+++ align.mk
@@ -1,6 +1,9 @@
 # This file contains the rules for aligning sequences. This allows alignment to
 # parallelized without having to process the full dependency list.
 
+RSCRIPT ?= Rscript --vanilla
+SHELL ?= /bin/bash -e -o pipefail
+
 COATI_BIN = ./bin/coati-alignpair
 PRANK_BIN = ./bin/prank
 MAFFT_BIN = ./bin/mafft
