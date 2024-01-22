090429e471dac01904dccfc2cee46d864930c8c1
Reed A. Cartwright <cartwright@asu.edu>
2024-01-21 18:53:25 -0700

    Update slurm submission scripts

--- slurm/run_align_test.bash
+++ slurm/run_align_test.bash
@@ -2,9 +2,9 @@
 
 #SBATCH -N 1            # number of nodes
 #SBATCH -c 1            # number of cores
-#SBATCH --mem=32G       # memory limit
-#SBATCH -t 6-00:00:00   # time in d-hh:mm:ss
-#SBATCH -p general      # partition
+#SBATCH -t 0-01:00:00   # time in d-hh:mm:ss
+#SBATCH --mem=16G       # memory limit
+#SBATCH -p htc          # partition
 #SBATCH -q public       # QOS
 #SBATCH -o logs/slurm.%j.out # file to save job's STDOUT (%j = JobId)
 #SBATCH -e logs/slurm.%j.err # file to save job's STDERR (%j = JobId)
@@ -18,9 +18,9 @@ stem=$1
 
 make -f align.mk "benchmark_fasta_aligned/coati-tri-mg/${stem}.coati-tri-mg.fasta"
 make -f align.mk "benchmark_fasta_aligned/coati-tri-ecm/${stem}.coati-tri-ecm.fasta"
+make -f align.mk "benchmark_fasta_aligned/coati-dna/${stem}.coati-dna.fasta"
 make -f align.mk "benchmark_fasta_aligned/coati-mar-mg/${stem}.coati-mar-mg.fasta"
 make -f align.mk "benchmark_fasta_aligned/coati-mar-ecm/${stem}.coati-mar-ecm.fasta"
-make -f align.mk "benchmark_fasta_aligned/coati-dna/${stem}.coati-dna.fasta"
 make -f align.mk "benchmark_fasta_aligned/rev-coati-tri-mg/${stem}.rev-coati-tri-mg.fasta"
 make -f align.mk "benchmark_fasta_aligned/mafft/${stem}.mafft.fasta"
 make -f align.mk "benchmark_fasta_aligned/macse/${stem}.macse.fasta"
--- slurm/sbatch_align_tests.bash
+++ slurm/sbatch_align_tests.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 find benchmark_fasta_nogaps -type f -name 'TEST*.fasta' -exec \
-    sh -c 'STEM=$(basename {} .nogaps.fasta); echo sbatch -J "align_${STEM}" sbatch/sbatch_align_tests.bash ${STEM}' \;
-
+    sh -c 'STEM=$(basename {} .nogaps.fasta); echo sbatch -J "align_${STEM}" slurm/run_align_test.bash ${STEM}' \;
