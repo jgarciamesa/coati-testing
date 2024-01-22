e0f56b5678f897779e30ccbcd207eec1e5a1a0af
Reed A. Cartwright <cartwright@asu.edu>
2024-01-22 00:50:45 -0700

    Add archives of aligned benchmark files

--- slurm/sbatch_align_tests.bash
+++ slurm/sbatch_align_tests.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find benchmark_fasta_nogaps -type f -name 'TEST*.fasta' -exec \
-    sh -c 'STEM=$(basename {} .nogaps.fasta); echo sbatch -J "align_${STEM}" slurm/run_align_test.bash ${STEM}' \;
+    sh -c 'STEM=$(basename {} .nogaps.fasta); sbatch -J "align_${STEM}" slurm/run_align_test.bash ${STEM}' \;
